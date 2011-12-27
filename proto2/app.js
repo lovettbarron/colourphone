1ca1b9c1f469ad663794b8c8af8eef8284bf4790
Andrew Lovett Barron <andrewlb@Andrew-Lovett-Barrons-MacBook-Air.local>
2011-12-26 19:34:33 -0500

    Socket sessioning

--- proto2/app.js
+++ proto2/app.js
@@ -279,7 +279,7 @@ function makeOAuth() {
 
 app.get('/friends', function(req, res) {
 	var response = '';
-	var friendIds = {};
+	var friendIds = { };
 
 	var oa = new OAuth('https://api.twitter.com/oauth/request_token'
 								, 'https://api.twitter.com/oauth/access_token'
@@ -311,7 +311,7 @@ app.get('/friends', function(req, res) {
 					response = docs;
 					for(var key in docs) {
 						if(docs.hasOwnProperty(key)){
-								friendIds[docs[key]._id] = docs[key].twit.id;
+								friendIds.(docs[key]._id) = docs[key].twit.id;
 							}
 						}
 					 console.log( "Friends list to be saved: " + JSON.stringify(friendIds) );
