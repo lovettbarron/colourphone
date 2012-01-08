b7d41126ca859a7cfdda8cdd15883b9f4da4ae30
Andrew Lovett Barron <andrewlb@Andrew-Lovett-Barrons-MacBook-Air.local>
2012-01-08 03:24:23 -0500

    Last bit, talking back

--- proto2/app.js
+++ proto2/app.js
@@ -269,7 +269,8 @@ io.sockets.on('connection', function (socket) {
 									}
 						console.log('Reply is ' + JSON.stringify(hs.session.reply));
 						socket.emit('update', hs.session.reply, function(err) {
-							hs.session.reply = [];
+							delete hs.session.reply;
+							hs.session.reply = new Array();
 							if(err) console.log('err sending update:'+err);
 							for( var key in hs.session.reply ) {
 								Colour.findOne({ '_id' : hs.session.reply[key]._id }, function(err, doc) {
