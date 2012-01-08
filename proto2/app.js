715619d82dc2b13ae0ef6ca9f2cac935a0893b18
Andrew Lovett Barron <andrewlb@Andrew-Lovett-Barrons-MacBook-Air.local>
2012-01-07 19:56:08 -0500

    Last bit, talking back

--- proto2/app.js
+++ proto2/app.js
@@ -249,10 +249,11 @@ io.sockets.on('connection', function (socket) {
 					if (friends.hasOwnProperty(key)) {
 						if(friends[key].id !== undefined ) {
 						console.log('Friend is ' + JSON.stringify(friends[key].id));	
-								var query = Colour.findOne({'colour.to' : userId
-									, 'colour.from' : friends[key].id });
-								query.sort({ '$natural': -1 })
-								query.exec(function(err2,doc2) {
+								var query = Colour.find({'colour.to' : userId
+									, 'colour.from' : friends[key].id })
+								.sort({ '$natural': -1 });
+								.limit(1);
+								.exec(function(err2,doc2) {
 										if(err) console.log("Err retrieving color:" + err + err2)
 										console.log('Returned colour:' + doc2);
 										if( doc2 !== undefined 
